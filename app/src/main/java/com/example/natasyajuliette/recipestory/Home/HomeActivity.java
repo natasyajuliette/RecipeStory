199319cda58990a8307ad856309f1244d4161dbc
natasya juliette <natasya.scirome@gmail.com>
2018-03-06 12:33:26 -0800

    Created the login layout and register layout

--- app/src/main/java/com/example/natasyajuliette/recipestory/Home/HomeActivity.java
+++ app/src/main/java/com/example/natasyajuliette/recipestory/Home/HomeActivity.java
@@ -55,7 +55,7 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
-        tabLayout.getTabAt(1).setIcon(R.drawable.ic_action_name);
+        tabLayout.getTabAt(1).setIcon(R.drawable.logo_figma);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_messages);
 
 
--- app/src/main/java/com/example/natasyajuliette/recipestory/Likes/LikeActivity.java
+++ app/src/main/java/com/example/natasyajuliette/recipestory/Likes/LikeActivity.java
@@ -26,9 +26,9 @@ public class LikeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_register);
         Log.d(TAG, "onCreate: started.");
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
 
     }
 
